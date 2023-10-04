65837998881dccfb17a3b5703645d9c44aa55650
beneroth13 <benediktroth1@web.de>
2023-10-04 13:08:00 +0000

    adapt dataset to match dino standards

--- dinov2/data/datasets/CustomImageDataset.py
+++ dinov2/data/datasets/CustomImageDataset.py
@@ -4,6 +4,7 @@ from torchvision.io import read_image
 from torch.utils.data import Dataset
 from torchvision import transforms
 from sklearn.model_selection import train_test_split
+from .decoders import TargetDecoder, ImageDataDecoder
 
 class CustomImageDataset(Dataset):
     def __init__(
@@ -21,20 +22,28 @@ class CustomImageDataset(Dataset):
         self.target_transform = target_transform
         self.split = split
 
-        # Aufteilen der Daten in Trainings- und Testsets
+        # split into train and test
         self.train_data, self.test_data = train_test_split(
             self.img_labels, test_size=test_size, random_state=random_state)
 
     def __len__(self):
-        return len(self.train_data)  # Anzahl der Trainingsdaten verwenden
+        return len(self.train_data) 
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.train_data.iloc[idx, 0])
-        image = read_image(img_path)
-        image_pil = transforms.ToPILImage()(image)
+        # read image
+        try:
+            img_path = os.path.join(self.img_dir, self.train_data.iloc[idx, 0])
+            with open(img_path, mode="rb") as f:
+                image_pil = f.read()
+            #image = read_image(img_path)
+            #image_pil = transforms.ToPILImage()(image)
+            image_pil = ImageDataDecoder(image_pil).decode()
+        except Exception as e:
+            raise RuntimeError(f"can not read image for sample {idx}") from e
         if self.transform:
             image_pil = self.transform(image_pil)
-        return image_pil
+
+        return image_pil, None
 
     def get_test_item(self, idx):
         img_path = os.path.join(self.img_dir, self.test_data.iloc[idx, 0])
