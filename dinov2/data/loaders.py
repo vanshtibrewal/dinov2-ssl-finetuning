46ee08b4a93d1f8e7340f1a9829f13441ad70825
beneroth13 <benediktroth1@web.de>
2023-10-04 08:51:32 +0000

    add custom dataset to loader

--- dinov2/data/loaders.py
+++ dinov2/data/loaders.py
@@ -11,7 +11,7 @@ from typing import Any, Callable, List, Optional, TypeVar
 import torch
 from torch.utils.data import Sampler
 
-from .datasets import ImageNet, ImageNet22k
+from .datasets import ImageNet, ImageNet22k, CustomImageDataset
 from .samplers import EpochSampler, InfiniteSampler, ShardedInfiniteSampler
 
 
@@ -60,8 +60,8 @@ def _parse_dataset_str(dataset_str: str):
             kwargs["split"] = ImageNet.Split[kwargs["split"]]
     elif name == "ImageNet22k":
         class_ = ImageNet22k
-    #elif name == "CustomDataset":
-        #class_ = CustomDataset
+    elif name == "CustomImageDataset":
+        class_ = CustomImageDataset
     else:
         raise ValueError(f'Unsupported dataset "{name}"')
 
