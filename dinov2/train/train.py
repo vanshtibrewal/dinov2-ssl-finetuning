eb2dc05eed937d8b2d2f3aed47fbecbd62b7eab2
beneroth13 <benediktroth1@web.de>
2023-10-04 14:53:38 +0000

    changed default config file

--- dinov2/train/train.py
+++ dinov2/train/train.py
@@ -37,7 +37,7 @@ logger = logging.getLogger("dinov2")
 
 def get_args_parser(add_help: bool = True):
     parser = argparse.ArgumentParser("DINOv2 training", add_help=add_help)
-    parser.add_argument("--config-file", default="/home/ubuntu/dinov2/dinov2/configs/train/vitl14.yaml", metavar="FILE", help="path to config file")
+    parser.add_argument("--config-file", default="/home/ubuntu/dinov2/dinov2/configs/ssl_default_config.yaml", metavar="FILE", help="path to config file") #/home/ubuntu/dinov2/dinov2/configs/ssl_default_config.yaml or /home/ubuntu/dinov2/dinov2/configs/train/vitl14.yaml
     parser.add_argument(
         "--no-resume",
         action="store_true",
