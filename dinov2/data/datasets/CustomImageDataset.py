f11d1aa8b479947f0e00d81a33a07a9a6a2e4c80
beneroth13 <benediktroth1@web.de>
2023-10-04 10:24:54 +0000

    add test and train split

--- dinov2/data/datasets/CustomImageDataset.py
+++ dinov2/data/datasets/CustomImageDataset.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from torchvision.io import read_image
 from torch.utils.data import Dataset
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
 
 class CustomImageDataset(Dataset):
     def __init__(
@@ -9,18 +11,35 @@ class CustomImageDataset(Dataset):
             root: str,
             extra: str,
             transform=None,
-            target_transform=None):
+            target_transform=None,
+            test_size=0.2,
+            random_state=42):
         self.img_labels = pd.read_csv(extra)
         self.img_dir = root
         self.transform = transform
         self.target_transform = target_transform
 
+        # Aufteilen der Daten in Trainings- und Testsets
+        self.train_data, self.test_data = train_test_split(
+            self.img_labels, test_size=test_size, random_state=random_state)
+
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.train_data)  # Anzahl der Trainingsdaten verwenden
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        img_path = os.path.join(self.img_dir, self.train_data.iloc[idx, 0])
         image = read_image(img_path)
+        image_pil = transforms.ToPILImage()(image)
         if self.transform:
-            image = self.transform(image)
-        return image
+            image_pil = self.transform(image_pil)
+        return image_pil
+
+    def get_test_item(self, idx):
+        img_path = os.path.join(self.img_dir, self.test_data.iloc[idx, 0])
+        image = read_image(img_path)
+        image_pil = transforms.ToPILImage()(image)
+        if self.transform:
+            image_pil = self.transform(image_pil)
+        return image_pil
+
+    
