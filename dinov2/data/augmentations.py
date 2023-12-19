9edad44bbc1b14fefdb30ff0ae90fb4ec2e9237a
beneroth13 <benediktroth1@web.de>
2023-12-19 15:49:49 +0100

    add remark that a change was made

--- dinov2/data/augmentations.py
+++ dinov2/data/augmentations.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the Apache License, Version 2.0
 # found in the LICENSE file in the root directory of this source tree.
 
+# this file was changed, the vertical flip was added
+
 import logging
 
 from torchvision import transforms
